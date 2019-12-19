f2cde66a7f6d972318ea82e35872e014af6549c0
unknown <gfram@hotmail.com>
2019-12-19 18:49:58 +0000

    Added more examples

---
+++ Variable Data Types.py
@@ -0,0 +1,13 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Numbers
+>>> x = 10
+>>> y = 10.341
+>>> z = 25j
+>>> print(x)
+10
+>>> print(y)
+10.341
+>>> print(z)
+25j
+>>> 
