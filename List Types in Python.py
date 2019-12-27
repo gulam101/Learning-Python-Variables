87a341866955263d0adefc28dc651a2da39001a2
unknown <gfram@hotmail.com>
2019-12-27 16:25:00 +0000

    Added List Example

---
+++ List Types in Python.py
@@ -0,0 +1,17 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Animals = ['Cat', 'Dog', 'Snake']
+>>> print(Animals)
+['Cat', 'Dog', 'Snake']
+>>> ### order cannot be changed
+>>> myNumbers = [10, 20, 30, 40]
+>>> print(myNumbers)
+[10, 20, 30, 40]
+>>> myNumbers.append(35)
+>>> print(myNumbers)
+[10, 20, 30, 40, 35]
+>>> ### Adding a value using the index number
+>>> myNumbers.insert(3, 120)
+>>> print(myNumbers)
+[10, 20, 30, 120, 40, 35]
+>>> 
